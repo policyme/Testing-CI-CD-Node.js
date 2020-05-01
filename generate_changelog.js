8ca932af7dc16f0218dc9e7cbd7c470b9fba45a6
Umang Gupta <umanggupta@Umangs-MacBook-Pro-2.local>
2020-04-30 23:41:17 -0400

    Updating the CHANGELOG.md and the _version file

--- generate_changelog.js
+++ generate_changelog.js
@@ -1,6 +1,6 @@
 function is_pull_request(commit){
     parse_array = commit.split(" ")
-        if(parse_array[0] == 'Merge' && parse_array[1] == 'pull' && parse_array[2] == 'request'){
+        if(parse_array[0] == 'Merge' && parse_array[1] == 'pull' && parse_array[2] == 'request' && parse_array[3] == '#3'){
             return true
         }
     return false
@@ -34,21 +34,29 @@ function gen_changelog(){
 
         var content = ''
 
-        content += '# ' + version + '(' + todayDate + ')' + '\n\n'
+        content += '# ' + version.version + ' (' + todayDate + ')' + '\n\n'
+
+        var ticketNumber = ''
 
         for (var i = 0; i < commits.length ; i++) {
             if (commits[i].subject != "Updating the CHANGELOG.md and the _version file"){
                 if(is_pull_request(commits[i].subject)){
-                    ticketNumber = commits[0].subject.split(" ")[5].split("/")[2].split("-")
+                    ticketNumber = commits[i].subject.split(" ")[5].split("/")[2].split("-")
                     ticketNumber = ticketNumber.slice(0,2).join("-")
                     ticket_set.add(ticketNumber)
                 }
+            }else{
+                break
             }
         }
 
-        console.log(ticket_set)
+        for(var it = ticket_set.values(), val= null; val=it.next().value;){
+            jira_link = 'https://policyme.atlassian.net/browse/' + val
+            content += '* ' + jira_link + '\n'
+            jira_link = ''
+        }
 
-        
+        content += '\n\n'
 
         const insert = new Buffer(content)
         fs.writeSync(fd, insert, 0, insert.length, 0)
