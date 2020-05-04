8061d59c086d7fcef6568c33ee5b04d7f3270794
Umang Gupta <umanggupta@Umangs-MacBook-Pro-2.local>
2020-05-04 16:39:38 -0400

    Merge pull request #3 from ugupta-pm/feature/ST-7899-develop

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,9 @@ if [ "$TRAVIS_BRANCH" == "feature/ST-1234-Testing" ]; then
     # Run the jest unit tests
     cd app/gui 
 
+    # Install all the node modules
+    npm install
+
     # Run the test command
     npm run test 
 
