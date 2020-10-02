212e1fbf6236f9d5ec0a0ed075554ceba92c3467
Gabriel Lee <gabriel.lee@policyme.com>
2020-10-02 17:49:52 -0400

    changelog should work now

--- pm_cicd/helpers.py
+++ pm_cicd/helpers.py
@@ -25,3 +25,13 @@ def exec_subprocess(cmd):
   print('exec_subprocess result:\n{}\n'.format(result.stdout))
   if result.returncode != 0:
     raise Exception("subprocess did not run successfully. See logs for details")
+
+# gets the ticket number from the string text
+def get_ticket_number(text):
+  parsed_message = text.split('-')
+  if len(parsed_message) > 1:
+    ticket_type = parsed_message[0].upper()
+    ticket_number = parsed_message[1]
+    if number.isdigit():
+      return ticket_type + '-' + ticket_number
+  return None
