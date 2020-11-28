976b85f84caf10730cdf16839bd6eb5a4a214995
Gabriel Lee <64923230+glee-pm@users.noreply.github.com>
2020-11-27 21:50:47 -0500

    try now (#58)

--- pm_cicd/utils/consts.py
+++ pm_cicd/utils/consts.py
@@ -27,12 +27,11 @@ class VERSION_SEMANTIC(enum.Enum):
   MAJOR = 'MAJOR'
 
 class COLORS:
-  HEADER = '\033[95m'
-  OKBLUE = '\033[94m'
-  OKCYAN = '\033[96m'
-  OKGREEN = '\033[92m'
-  WARNING = '\033[93m'
-  FAIL = '\033[91m'
+  BLUE = '\033[94m'
+  CYAN = '\033[96m'
+  GREEN = '\033[92m'
+  YELLOW = '\033[93m'
+  RED = '\033[91m'
   ENDC = '\033[0m'
   BOLD = '\033[1m'
   UNDERLINE = '\033[4m'
